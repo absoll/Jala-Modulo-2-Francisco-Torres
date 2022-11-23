46c84eb8d95ca24c97e1dcd3c850880dcc3c46bf
Francisco Torres <uzumakijunior@gmail.com>
2022-11-22 23:19:13 -0300

    Aula 2: Objetos e colisao
    Adicionado objetos no mapa e colisão do player com eles

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,8 +1,10 @@
 import pygame
+
 from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
 
 X_POS = 80
 Y_POS = 310
+Y_POS_DUCK = 340
 JUMP_VEL = 8.5
 
 class Dinosaur:
@@ -36,12 +38,9 @@ class Dinosaur:
 
     def duck(self):
         self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
-        self.dino_rect.y = Y_POS + 30
+        self.dino_rect.y = Y_POS_DUCK
         self.step_index += 1
-
-        if self.step_index >= 10:
-            self.dino_duck = False
-
+        self.dino_duck = False
 
     def update(self, user_input):
         if self.dino_run:
@@ -51,14 +50,17 @@ class Dinosaur:
         elif self.dino_duck:
             self.duck()
 
-        if user_input[pygame.K_UP] and self.dino_run:
+        if user_input[pygame.K_UP] and not self.dino_jump:
             self.dino_jump = True
+            self.dino_duck = False
             self.dino_run = False
-        elif user_input[pygame.K_DOWN] and self.dino_run:
-            self.step_index = 0
+        elif user_input[pygame.K_DOWN] and not self.dino_jump:
+            self.dino_jump = False
             self.dino_duck = True
             self.dino_run = False
         elif (not self.dino_jump and not self.dino_duck):
+            self.dino_jump = False
+            self.dino_duck = False
             self.dino_run = True
 
 
